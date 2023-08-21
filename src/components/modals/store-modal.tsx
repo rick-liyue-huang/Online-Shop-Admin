269941e95bb0f81e65e46d1ab9190c94f9d3cbf3
Rick Huang <rick.liyue.huang@gmail.com>
2023-08-21 13:53:12 +1000

    feat: add form to create store

--- src/components/modals/store-modal.tsx
+++ src/components/modals/store-modal.tsx
@@ -2,9 +2,37 @@
 
 import { useStoreModal } from '@/hooks/use-store.modal';
 import { Modal } from '@/components/Modal';
+import * as z from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+
+const formSchema = z.object({
+  name: z.string().min(3).max(255),
+});
 
 export const StoreModal = () => {
   const storeModal = useStoreModal();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: '',
+    },
+  });
+
+  const onSubmit = (data: z.infer<typeof formSchema>) => {
+    console.log(data);
+  };
 
   return (
     <Modal
@@ -13,7 +41,34 @@ export const StoreModal = () => {
       isOpen={storeModal.isOpen}
       onClose={storeModal.onClose}
     >
-      Create Store Form
+      <div className="space-y-4 py-2 pb-4">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name: </FormLabel>
+                  <FormControl>
+                    <Input placeholder="Online Shop" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+              <Button variant={'destructive'} onClick={storeModal.onClose}>
+                Cancel
+              </Button>
+              <Button variant={'outline'} type="submit">
+                Continue
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </div>
     </Modal>
   );
 };
