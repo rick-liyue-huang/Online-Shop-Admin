b773cb74b4966c628767d4fb016ddbd056c47698
Rick Huang <rick.liyue.huang@gmail.com>
2023-08-21 15:21:21 +1000

    feat: add prisma on project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ClerkProvider } from '@clerk/nextjs';
 import { ModalProvider } from '@/providers/modalProvider';
+import prismaDB from '../lib/prisma-db';
 
 const inter = Inter({ subsets: ['latin'] });
 
